08a20e57da82cc0924b6e327e21a66d0ecd31e64
Peter Thestrup Waade <peter@waade.net>
2025-03-24 16:59:42 +0100

    made index be in the right place

--- docs/make.jl
+++ docs/make.jl
@@ -17,20 +17,30 @@ theory_folder = joinpath(markdown_files_folder, "theory")
 
 ## GENERATE MARKDOWNS ##
 #Remove old markdowns
-for markdown_file in glob( "*.md", generated_files_folder)
+for markdown_file in glob("*.md", generated_files_folder)
     rm(markdown_file)
 end
 
 #Create markdowns from julia files
-for julia_file in [glob( "*/*.jl", julia_files_folder); glob( "*.jl", julia_files_folder)]
+for julia_file in glob("*/*.jl", julia_files_folder)
 
-    Literate.markdown(julia_file,
-                      generated_files_folder, 
-                      execute=true, 
-                      documenter=true,
-                      codefence =  "```julia" => "```"
-                      )
+    Literate.markdown(
+        julia_file,
+        generated_files_folder,
+        execute = true,
+        documenter = true,
+        codefence = "```julia" => "```",
+    )
 end
+
+#Including the index file 
+Literate.markdown(
+    joinpath(julia_files_folder, "index.jl"),
+    markdown_files_folder,
+    execute = true,
+    documenter = true,
+    codefence = "```julia" => "```",
+)
 
 
 ## GENERATE AND DEPLOY DOCS ##
@@ -53,9 +63,10 @@ makedocs(;
         assets = String[],
         size_threshold = 1_500_000, ##MAKE THIS SMALLER?
     ),
-    doctest=true,
+    doctest = true,
     pages = [
-        "Introduction to Hierarchical Gaussian Filtering" => joinpath(".", "generated", "index.md"),
+        "Introduction to Hierarchical Gaussian Filtering" =>
+            joinpath(".", "index.md"),
         # "Theory" => [
         #     "./theory" * "/genmodel.md",
         #     "./theory" * "/node.md",
@@ -63,13 +74,14 @@ makedocs(;
         #     "./theory" * "/vope.md",
         # ],
         "Using the package" => [
-            
             "The HGF Nodes" => joinpath(".", "generated", "the_HGF_nodes.md"),
             "Building an HGF" => joinpath(".", "generated", "building_an_HGF.md"),
             "Updating the HGF" => joinpath(".", "generated", "updating_the_HGF.md"),
-            "List Of Premade Agent Models" => joinpath(".", "generated", "premade_models.md"),
+            "List Of Premade Agent Models" =>
+                joinpath(".", "generated", "premade_models.md"),
             "List Of Premade HGF's" => joinpath(".", "generated", "premade_HGF.md"),
-            "Fitting an HGF-agent model to data" => joinpath(".", "generated", "fitting_hgf_models.md"),
+            "Fitting an HGF-agent model to data" =>
+                joinpath(".", "generated", "fitting_hgf_models.md"),
             "Utility Functions" => joinpath(".", "generated", "utility_functions.md"),
         ],
         "Tutorials" => [
