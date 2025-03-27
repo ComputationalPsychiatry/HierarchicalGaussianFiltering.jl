940eb1b24e54b524ff9cb9ace1bcadbbc759034b
Peter Thestrup Waade <peter@waade.net>
2025-03-27 21:17:17 +0100

    minor update to tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,15 +7,15 @@ hgf_path = dirname(dirname(pathof(HierarchicalGaussianFiltering)))
 
 @testset "All tests" begin
 
+    #Get the path to the testing folder
+    test_path = hgf_path * "/test/"
+
+    @testset "quick tests" begin
+        # Test the quick tests that are used as pre-commit tests
+        include(test_path * "quicktests.jl")
+    end
+
     @testset "Unit tests" begin
-
-        #Get the path to the testing folder
-        test_path = hgf_path * "/test/"
-
-        @testset "quick tests" begin
-            # Test the quick tests that are used as pre-commit tests
-            include(test_path * "quicktests.jl")
-        end
 
         # List the julia filenames in the testsuite
         filenames = glob("*.jl", test_path * "testsuite")
@@ -32,28 +32,13 @@ hgf_path = dirname(dirname(pathof(HierarchicalGaussianFiltering)))
         #Set up path for the documentation folder
         documentation_path = joinpath(hgf_path, "docs", "julia_files")
 
-        @testset "Sourcefiles" begin
+        # List the julia filenames in the documentation source files folder
+        filenames = [glob("*/*.jl", documentation_path); glob("*.jl", documentation_path)] 
 
-            # List the julia filenames in the documentation source files folder
-            filenames = [glob("*/*.jl", documentation_path); glob("*.jl", documentation_path)] 
-
-            for filename in filenames
-                @testset "$(splitpath(filename)[end])" begin
-                    include(filename)
-                end
+        for filename in filenames
+            @testset "$(splitpath(filename)[end])" begin
+                include(filename)
             end
         end
-
-        # @testset "Tutorials" begin
-
-        #     # List the julia filenames in the tutorials folder
-        #     filenames = glob("*.jl", joinpath(documentation_path, "tutorials"))
-
-        #     for filename in filenames
-        #         @testset "$filename" begin
-        #             include(filename)
-        #         end
-        #     end
-        # end
     end
 end
